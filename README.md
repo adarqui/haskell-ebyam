17223c6fbd132d02b6cf31169601a8ae8195d978
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-28 00:23:27 -0400

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# purescript-ebyam
+
+![Ebyam Mascot](/assets/mascot.jpg?raw=true "Ebyam Mascot")
+
+Optional values. `Maybe` is often used to capture failures and in cases where nullable values might otherwise have been used in other languages.
+
+Basically a copy of my purescript-ebyam. Turns out I enjoy using ebyam in Haskell too.
+
+## Installation
+
+```
+stack install haskell-ebyam
+```
---
+++ src/Data/Ebyam.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ExplicitForAll    #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Data.Ebyam (
+  ebyam
+) where
+
+
+
+import           Data.Maybe (Maybe, maybe)
+
+
+
+-- | Takes a `Maybe` value, a default value, and a function. If the `Maybe`
+-- | value is `Nothing` the default value is returned, otherwise the function
+-- | is applied to the value inside the `Just` and the result is returned.
+-- |
+-- | ``` purescript
+-- | ebyam Nothing x f == x
+-- | ebyam (Just y) x f == f y
+-- | ```
+
+ebyam :: forall a b. Maybe a -> b -> (a -> b) -> b
+ebyam m_a b f = maybe b f m_a
